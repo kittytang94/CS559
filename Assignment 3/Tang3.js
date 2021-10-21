98cd1a50523e2b20c7965d84deada5fc4b6410bb
kittytang94 <lptang@wisc.edu>
2021-10-21 10:35:53 -0500

    working fish and bubbles

--- Assignment 3/Tang3.js
+++ Assignment 3/Tang3.js
@@ -2,12 +2,7 @@ function setup() {
     "use strict";
     var canvas = document.getElementById('Tang3');
     var context = canvas.getContext('2d');
-
-    var slider = document.getElementById('slider');
-    slider.value = 0;
-
     canvas.style.background = "powderblue";
-    var tParam = slider.value * 0.01;
 
     function draw() {
         context.clearRect(0, 0, context.canvas.width, canvas.height);
@@ -17,88 +12,69 @@ function setup() {
             vec2.transformMat3(res, loc, Tx);
             context.moveTo(res[0], res[1]);
         }
-//adjust to make a curve instead of line (use either this or hermite curve)
+
         function lineToTx(loc, Tx) {
             var res = vec2.create();
             vec2.transformMat3(res, loc, Tx);
             context.lineTo(res[0], res[1]);
         }
 
-        var Hermite = function (t) {
-            return [
-                2 * Math.pow(t, 3) - 3 * Math.pow(t, 2) + 1,
-                Math.pow(t, 3) - 2 * Math.pow(t, 2) + t,
-                -2 * Math.pow(t, 3) + 3 * Math.pow(t, 2),
-                Math.pow(t, 3) - Math.pow(t, 2)
-            ];
+        //draws ellipse
+        function ellipseTx(loc, Tx ) {
+            var res = vec2.create();
+            vec2.transformMat3(res, loc, Tx);
+            context.ellipse(res[0], res[1], 20, 10, 0, 0, 2 * Math.PI);
         }
-//need to somehow understand this more
-        function Cubic(basis, P, t) {
-            var b = basis(t);
-            var result = vec2.create();
-            vec2.scale(result, P[0], b[0]);
-            vec2.scaleAndAdd(result, result, P[1], b[1]);
-            vec2.scaleAndAdd(result, result, P[2], b[2]);
-            //vec2.scaleAndAdd(result, result, P[3], b[3]);
-            //vec2.scaleAndAdd(result, result, P[3], b[3]);
-            return result;
+
+        //draws cirle
+        function arcTx(rad, loc, Tx) {
+        var res = vec2.create();
+        vec2.transformMat3(res, loc, Tx);
+        context.arc(res[0], res[1], rad, 0, 2*Math.PI);
         }
-//need to adjust points to be in line with a fish
-        var p0 = [0, 0];
-        var v0 = [2, 2];
-        var p1 = [2, 2.5];
-        var v1 = [1, 1];
-        var p2 = [0, 0];
-        var v2 = [1, 1];
 
-        var P0 = [p0, v0, p1, v1]; // First two points and tangents
-        var P1 = [p1, v1, p2, v2]; // Last two points and tangents
-
-        var C0 = function (t_) { return Cubic(Hermite, P0, t_); };
-        var C1 = function (t_) { return Cubic(Hermite, P1, t_); };
-       // var C2 = function (t_) { return Cubic(Hermite, P2, t_); };
-
-//need to change this to allow for window animation movement back and forth rather than slider
-        var anim = function (t) {
-            if (t < 1) {
-                var u = t;
-                return C0(u);
-            } else {
-                var u = t - 1.0;
-                return C1(u);
-            }
-        }
-        
-        //draws one line of the fish
-        function drawPath(t_begin, t_end, intervals, C, Tx, color) {
-            context.strokeStyle = color;
-            context.lineWidth = 3;
+        //draw bubble
+        function drawBubble(Tx) {
+            context.strokeStyle = "white"
+            moveToTx([0,0], Tx);
             context.beginPath();
-            moveToTx(C(t_begin), Tx);
-            for (var i = 1; i <= intervals; i++) {
-                var t = ((intervals - i) / intervals) * t_begin + (i / intervals) * t_end;
-                lineToTx(C(t), Tx);
-            }
+            arcTx(20, [0,0], Tx);
+            context.closePath();
+            context.stroke();
+            moveToTx([7,-9], Tx);
+            context.beginPath();
+            arcTx(2, [7,-9], Tx);
+            context.closePath();
+            context.stroke();
+            moveToTx([10,-5], Tx);
+            context.beginPath();
+            arcTx(2, [10,-5], Tx);
+            context.closePath();
             context.stroke();
         }
+
         //draws whole fish
-        function drawFish(color) {
-            var fish1 = mat3.create();
-            mat3.fromTranslation(fish1, [500, 250]);
-            drawPath(0, 2.5, 100, C0, fish1, "red");
-            
-            drawPath(10, 5, 100, C1, fish1, "blue");
+        function drawFish(color,Tx) {
+            context.beginPath();
+            context.fillStyle = color;
+            context.strokeStyle = color;
+            moveToTx([0,0],Tx);
+            ellipseTx([0,0], Tx);
+            moveToTx([20,0], Tx);
+            lineToTx([30,10], Tx);
+            lineToTx([35,-10],Tx);
+            context.stroke();
+            context.fill();
+
         }
-
-
-
-        drawFish();
-
-        var isThisFish = mat3.create();
-        mat3.fromTranslation(isThisFish, anim(tParam));
+        var fish1 = mat3.create();
+        mat3.fromTranslation(fish1, [500, 250]);
+        drawFish("blue", fish1);
+        var bubble = mat3.create();
+        mat3.fromTranslation(bubble, [100, 100]);
+        drawBubble(bubble);
     }
-    draw();
-    //window.requestAnimationFrame(draw);
+    window.requestAnimationFrame(draw);
 }
 window.onload = setup;
 
