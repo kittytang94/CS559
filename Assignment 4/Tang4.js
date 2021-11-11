8d93687d68802adb9f94abeddda5e3f62e2d4c97
kittytang94 <lptang@wisc.edu>
2021-11-10 22:34:49 -0600

    it kinda works?

--- Assignment 4/Tang4.js
+++ Assignment 4/Tang4.js
@@ -4,16 +4,12 @@ function setup() {
     var canvas = document.getElementById('Tang4');
     var context = canvas.getContext('2d');
     canvas.style.background = "powderblue";
-
-<<<<<<< HEAD
     var beeT = 0;
 
-=======
->>>>>>> 04b04a7bc5ca0066aeff9aaea54e816f920995a2
     function draw() {
         context.clearRect(0, 0, canvas.width, canvas.height);
-        beeT +=0.01;
-        if(beeT == 2) { beeT = 0;}
+        beeT +=.01;
+        if(beeT == 1.0) { beeT = 0; drawBee(bee);}
 
         function moveToTx(loc, Tx) {
             var res = vec2.create();
@@ -34,10 +30,7 @@ function setup() {
             vec2.transformMat3(res, loc, Tx);
             context.ellipse(res[0], res[1], xRad, 5, rotation, 0, 2 * Math.PI);
         }
-<<<<<<< HEAD
-=======
 
->>>>>>> 04b04a7bc5ca0066aeff9aaea54e816f920995a2
         function drawHive(Tx) {
             context.lineWidth = 10;
             context.fillStyle = "gold";
@@ -60,7 +53,6 @@ function setup() {
             moveToTx([0, 195], Tx); ellipseTx([0, 195], 75, Tx);
             moveToTx([0, 210], Tx); ellipseTx([0, 210], 60, Tx);
             context.stroke(); context.fill(); context.closePath();
-
         }
         function drawBee(Tx) {
             context.fillStyle = "yellow";
@@ -78,6 +70,7 @@ function setup() {
             moveToTx([-1, -1], Tx);
             context.beginPath();
             ellipseTx([7, -10], 12, Tx, 3 * Math.PI / 4);
+            context.closePath();
             context.fill();
         }
 
@@ -92,12 +85,6 @@ function setup() {
                 lineToTx(C(t), Tx);
             }
             context.stroke();
-        }
-
-        function bezier(cp1x, cp1y, cp2x, cp2y, loc,Tx) {
-            var res = vec2.create();
-            vec2.transformMat3(res, loc, Tx);
-            context.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, res[0], res[1]);
         }
 
 	    var Hermite = function(t) {
@@ -119,30 +106,24 @@ function setup() {
             return result;
         }
 
-        var p0=[-50,50]; var d0=[-50,-30];
-        var p1=[-1,-1]; var d1=[5,-3];
-        var p2=p1; var d2=[-51,20];
-        var p3=p2; var d3=[5,5];
+        var p0=[200,200]; var d0=[100, 350];
+        var p1=[100,150]; var d1=[350,400];
+        var p2=[200, 250]; var d2=[400,0];
+
 
         var P0 = [p0,d0,p1,d1]; // First two points and tangents
         var P1 = [p1,d1,p2,d2]; // Last two points and tangents
-        var P2 = [p2,d2,p3,d3];
 
-    
         var C0 = function(t_) {return Cubic(Hermite,P0,t_);};
         var C1 = function(t_) {return Cubic(Hermite,P1,t_);};
-        var C2 = function(t_) {return Cubic(Hermite, P2, t_)};
 
         var Ccomp = function(t) {
             if (t<1){
                 var u = t;
                 return C0(u);
-            } else if (t<2){
+            } else {
                 var u = t-1.0;
                 return C1(u);
-            } else {
-                var u = t-2.0;
-                return C2(u);
             }          
         }
 
@@ -151,22 +132,18 @@ function setup() {
         drawHive(hive);
 
         var path = mat3.create();
-        mat3.fromTranslation(path, [250,250]);
-        mat3.scale(path, path, [1,-1]);
-
+        mat3.fromTranslation(path, [50,350]);
+        mat3.scale(path, path, [150,-150]);
 
         drawTrajectory(0.0,1.0, 100, C0, path, "red");
         drawTrajectory(1.0, 2.0, 100, C1, path, "blue");
-        drawTrajectory(2.0,3.0,100, C2, path, "purple");
 
-        var what = mat3.create();
-        mat3.fromTranslation(what, Ccomp(beeT));
         var bee = mat3.create();
-        mat3.multiply(bee, path, what);
-        mat3.fromTranslation(bee, [250, 300]);
+        mat3.fromTranslation(bee, Ccomp(beeT));
         drawBee(bee);
     }
 
-    setInterval(draw, 25);
+    setInterval(draw, 75);
+    //window.requestAnimationFrame(draw);
 }
 window.onload = setup;
